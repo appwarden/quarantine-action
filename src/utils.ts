0756015fb2e5ea7ab3f49bb2458160469642b170
stevefrench39 <stevefrench39@protonmail.com>
2024-06-18 15:34:23 -0600

    init

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,10 @@
-import { existsSync } from "node:fs";
-import * as path from "node:path";
-import semverGt from "semver/functions/gt";
+const protocolRegex = /^https?:\/\//i
 
-/**
- * A helper function to compare two semver versions. If the second arg is greater than the first arg, it returns true.
- */
-export function semverCompare(version1: string, version2: string) {
-	if (version2 === "latest") return true;
-
-	return semverGt(version2, version1);
-}
-
-export function checkWorkingDirectory(workingDirectory = ".") {
-	const normalizedPath = path.normalize(workingDirectory);
-	if (existsSync(normalizedPath)) {
-		return normalizedPath;
-	} else {
-		throw new Error(`Directory ${workingDirectory} does not exist.`);
+export const ignoreProtocol = (maybeFQDN: string) => {
+	const hasProtocol = protocolRegex.test(maybeFQDN)
+	if (hasProtocol) {
+		return maybeFQDN.replace(protocolRegex, "")
 	}
+
+	return maybeFQDN
 }
